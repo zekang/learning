785af14f7deee6eaf3a766bfe77017b53d0eced5
wanghouqian <wanghouqian@xywy.com>
2016-06-02 18:42:41 +0800

    fastcgi

---
+++ simple-webserver/fastcgi.c
@@ -0,0 +1,143 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"fastcgi.h"
+#include<WinSock2.h>
+#pragma comment(lib,"ws2_32.lib")
+
+int buildPacket(char type, char *content /*in*/,int content_len, unsigned short requestId, char *retval /*out*/,int *retval_len/*out*/)
+{
+	fcgi_header head;
+	int offset = 0;
+	int retval_offset = 0;
+	int packet_content_len = 0;
+	int is_end;
+	*retval_len = 0;
+	do{
+		if (content_len - offset < 65535 - FCGI_HEADER_LEN){
+			packet_content_len = content_len - offset;
+			is_end = 1;
+		}
+		else{
+			packet_content_len = 65535 - FCGI_HEADER_LEN;
+			is_end = 0;
+		}
+
+		head.version = FCGI_VERSION_1;
+		head.type = type;
+		head.requestIdB1 = requestId >> 8 & 0XFF;
+		head.requestIdB0 = requestId & 0XFF;
+		head.contentLengthB1 = packet_content_len >> 8 & 0xFF;
+		head.contentLengthB0 = packet_content_len & 0XFF;
+		head.paddingLength = 0;
+		head.reserved = 0;
+		memcpy(retval + retval_offset, &head, FCGI_HEADER_LEN);
+		retval_offset += FCGI_HEADER_LEN;
+		if (packet_content_len > 0){
+			memcpy(retval + retval_offset, content + offset, packet_content_len);
+			retval_offset += packet_content_len;
+			offset += packet_content_len;
+		}
+		*retval_len += FCGI_HEADER_LEN + packet_content_len;
+		if (is_end){
+			break;
+		}
+	} while (1);
+	return 0;
+}
+
+int buildNvpair(char *name /*in*/, char *value /*in*/, char *retval /*out*/,int *retval_len /*out*/)
+{
+	int name_len = strlen(name);
+	int value_len = strlen(value);
+	int index = 0;
+	
+	if (name_len < 128){
+		retval[index++] = name_len;
+	}
+	else{
+		retval[index++] = name_len >> 24 | 0X80;
+		retval[index++] = name_len >> 16 | 0XFF;
+		retval[index++] = name_len >> 8  | 0XFF;
+		retval[index++] = name_len | 0XFF;
+	}
+
+	if (value_len < 128){
+		retval[index++] = value_len;
+	}
+	else{
+		retval[index++] = value_len >> 24 | 0X80;
+		retval[index++] = value_len >> 16 | 0XFF;
+		retval[index++] = value_len >> 8 | 0XFF;
+		retval[index++] = value_len | 0XFF;
+	}
+	memcpy(retval + index, name, name_len);
+	index += name_len;
+	memcpy(retval + index, value, value_len);
+	index += value_len;
+	*retval_len = index;
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	struct sockaddr_in serv_addr;
+	SOCKET socketCli;
+	char sendBuf[204800], buf[102400] = { 0 };
+	char content[] = { 0, FCGI_RESPONDER, 0, 0, 0, 0, 0, 0 };
+	char paramBuf[1024];
+	int buf_len, pos=0,param_pos=0;
+	fcgi_header head = { 0 };
+	int response_len = 0;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData)){
+		return -1;
+	}
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	serv_addr.sin_port = htons(9000);
+	serv_addr.sin_family = AF_INET;
+	socketCli = socket(AF_INET, SOCK_STREAM, 0);
+	connect(socketCli, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+ 
+	buildPacket(FCGI_BEGIN_REQUEST, content , sizeof(content), 1, buf, &buf_len);
+	memcpy(sendBuf + pos, buf, buf_len);
+	pos += buf_len;
+	
+	buildNvpair("REQUEST_METHOD", "GET", buf, &buf_len);
+	memcpy(paramBuf + param_pos, buf, buf_len);
+	param_pos += buf_len;
+
+	buildNvpair("SCRIPT_FILENAME", "d:/www/test.php", buf, &buf_len);
+	memcpy(paramBuf + param_pos, buf, buf_len);
+	param_pos += buf_len;
+
+	buildPacket(FCGI_PARAMS, paramBuf, param_pos, 1, buf, &buf_len);
+	memcpy(sendBuf + pos, buf, buf_len);
+	pos += buf_len;
+
+	buildPacket(FCGI_PARAMS, "", 0, 1, buf, &buf_len);
+	memcpy(sendBuf + pos, buf, buf_len);
+	pos += buf_len;
+	
+	buildPacket(FCGI_STDIN, "", 0, 1, buf, &buf_len);
+	memcpy(sendBuf + pos, buf, buf_len);
+	pos += buf_len;
+
+	send(socketCli, sendBuf, pos, 0);
+	do{
+		if (recv(socketCli, (char *)&head, sizeof(head), 0) == 0){
+			break;
+		}
+
+		response_len = ((head.contentLengthB1) << 8) + head.contentLengthB0;
+		recv(socketCli, buf, response_len, 0);
+		buf[response_len] = 0;
+		printf("%s",buf);
+		if (head.type == FCGI_END_REQUEST){
+			break;
+		}
+	} while (1);
+	system("pause");
+	return 0;
+}
---
+++ simple-webserver/fastcgi.h
@@ -0,0 +1,49 @@
+#ifndef __FCGI_H__
+#define __FCGI_H__
+
+#define FCGI_VERSION_1             1
+// Packet types
+#define FCGI_BEGIN_REQUEST         1
+#define FCGI_ABORT_REQUEST         2
+#define FCGI_END_REQUEST           3
+#define FCGI_PARAMS                4
+#define FCGI_STDIN                 5
+#define FCGI_STDOUT                6
+#define FCGI_STDERR                7
+#define FCGI_DATA                  8
+#define FCGI_GET_VALUES            9
+#define FCGI_GET_VALUES_RESULT     10
+#define FCGI_UNKNOWN_TYPE          11
+#define FCGI_RESPONDER             1
+#define FCGI_AUTHORIZER            2
+#define FCGI_FILTER                3
+// Response codes
+#define FCGI_REQUEST_COMPLETE      0
+#define FCGI_CANT_MPX_CONN         1
+#define FCGI_OVERLOADED            2
+#define FCGI_UNKNOWN_ROLE          3
+// Number of bytes used in FastCGI header packet
+#define FCGI_HEADER_LEN            8
+
+typedef struct _fcgi_header {
+	unsigned char version;
+	unsigned char type;
+	unsigned char requestIdB1;
+	unsigned char requestIdB0;
+	unsigned char contentLengthB1;
+	unsigned char contentLengthB0;
+	unsigned char paddingLength;
+	unsigned char reserved;
+} fcgi_header;
+
+typedef struct _fcgi_params{
+	char key[64];
+	char value[512];
+}fcgi_params;
+
+int buildPacket(char type, char *content /*in*/, int content_len, unsigned short requestId, char *retval /*out*/, int *retval_len/*out*/);
+
+int buildNvpair(char *name /*in*/, char *value /*in*/, char *retval /*out*/, int *retval_len /*out*/);
+
+
+#endif
